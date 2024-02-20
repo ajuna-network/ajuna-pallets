1d0ca0b695f891c42002edca67288dbfbe420cdf
Dídac Sementé Fernández <didac@ajuna.io>
2024-02-20 13:31:55 +0100

    * Removed v5 migration

--- pallets/ajuna-awesome-avatars/src/migration/mod.rs
+++ pallets/ajuna-awesome-avatars/src/migration/mod.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pub mod v5;
 pub mod v6;
 
 use super::*;
