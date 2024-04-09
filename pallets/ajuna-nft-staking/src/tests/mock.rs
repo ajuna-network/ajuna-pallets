d5bc47f6e52872f6df2ace5aecfd2f69cb0d8cd2
clangenb <37865735+clangenb@users.noreply.github.com>
2024-04-09 15:44:51 +0800

    update to polkadot v1.9.0 and switch to crates.io dependencies (#35)

--- pallets/ajuna-nft-staking/src/tests/mock.rs
+++ pallets/ajuna-nft-staking/src/tests/mock.rs
@@ -81,6 +81,11 @@ impl frame_system::Config for Test {
 	type Nonce = MockNonce;
 	type Block = MockBlock;
 	type RuntimeTask = RuntimeTask;
+	type SingleBlockMigrations = ();
+	type MultiBlockMigrator = ();
+	type PreInherents = ();
+	type PostInherents = ();
+	type PostTransactions = ();
 }
 
 parameter_types! {
