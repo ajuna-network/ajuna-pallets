cefae7cb3814d958545f39ceb848e5f7f111aeed
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-04-21 15:55:54 +0200

    [aaa/migration] fix make v6 public

--- pallets/ajuna-awesome-avatars/src/migration/mod.rs
+++ pallets/ajuna-awesome-avatars/src/migration/mod.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-mod v6;
+pub mod v6;
 
 use super::*;
 use frame_support::traits::OnRuntimeUpgrade;
