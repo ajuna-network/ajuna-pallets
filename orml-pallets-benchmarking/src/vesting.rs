678f4b1931dba3adb9b7dedd27724f7f1a6b340f
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-04-25 15:10:10 +0200

    [orml-pallets-benchmarking] fix wrong licence header

--- orml-pallets-benchmarking/src/vesting.rs
+++ orml-pallets-benchmarking/src/vesting.rs
@@ -1,20 +1,18 @@
-// This file is part of Acala.
-
-// Copyright (C) 2020-2024 Acala Foundation.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+// Ajuna Node
+// Copyright (C) 2022 BlogaTech AG
 
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use super::utils::{
 	get_vesting_account, lookup_of_account, set_balance, AccountIdFor, BalanceFor, CurrencyFor,
