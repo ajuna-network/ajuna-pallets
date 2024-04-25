9c4b7c5712f0724e05223c2dccc36267489b1d75
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-04-25 13:32:23 +0200

    [orml-pallets-benchmarking] fix no-std build

--- orml-pallets-benchmarking/src/lib.rs
+++ orml-pallets-benchmarking/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 pub mod mock;
 pub mod utils;
 pub mod vesting;
