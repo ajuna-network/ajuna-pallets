2196277294416e6a1b71e7bd0707548585c96143
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-04-25 14:13:07 +0200

    [orml-pallets-benchmarking] fix benchmark discovery in node

--- orml-pallets-benchmarking/src/lib.rs
+++ orml-pallets-benchmarking/src/lib.rs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg(feature = "runtime-benchmarks")]
 
 pub mod mock;
 pub mod utils;
