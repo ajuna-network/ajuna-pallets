3206b69be9a626946325afa55984cfa499c01a08
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-10-01 11:00:16 +0200

    only use generic asset trait where it makes sense.

--- sage/sage-api/src/example_transitions.rs
+++ sage/sage-api/src/example_transitions.rs
@@ -2,13 +2,13 @@
 //!
 //! These should be expanded to really showcase the power of the SageApi design.
 
-use crate::{primitives, sage::SageApi};
+use crate::{primitives, primitives::Asset, sage::SageApi};
 use frame_support::ensure;
 
 /// Verifies a transition rule with a given transition id.
-pub fn verify_transition_rule<Sage: SageApi>(
+pub fn verify_transition_rule<Sage: SageApi<Asset = Asset>>(
 	transition_id: u32,
-	assets: &[Sage::Asset],
+	assets: &[Asset],
 ) -> Result<(), primitives::Error> {
 	match transition_id {
 		1 => rule_asset_length_1::<Sage>(assets),
@@ -17,9 +17,9 @@ pub fn verify_transition_rule<Sage: SageApi>(
 }
 
 /// Executes a transition with a given transition id.
-pub fn transition<Sage: SageApi>(
+pub fn transition<Sage: SageApi<Asset = Asset>>(
 	transition_id: u32,
-	assets: Vec<Sage::Asset>,
+	assets: Vec<Asset>,
 ) -> Result<(), primitives::Error> {
 	match transition_id {
 		1 => transition_one::<Sage>(assets),
@@ -28,12 +28,16 @@ pub fn transition<Sage: SageApi>(
 }
 
 /// One specific transition that a game wants to execute.
-pub fn transition_one<Sage: SageApi>(_assets: Vec<Sage::Asset>) -> Result<(), primitives::Error> {
+pub fn transition_one<Sage: SageApi<Asset = Asset>>(
+	_assets: Vec<Asset>,
+) -> Result<(), primitives::Error> {
 	todo!()
 }
 
 /// A rule that maybe many different transitions want to fulfill.
-pub fn rule_asset_length_1<Sage: SageApi>(assets: &[Sage::Asset]) -> Result<(), primitives::Error> {
+pub fn rule_asset_length_1<Sage: SageApi<Asset = Asset>>(
+	assets: &[Asset],
+) -> Result<(), primitives::Error> {
 	ensure!(assets.len() == 1, primitives::Error::InvalidAssetLength);
 	Ok(())
 }
