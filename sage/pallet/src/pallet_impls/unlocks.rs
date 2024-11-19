5134e4e876231e3359cec280dcdb8deeb7034cd3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-11-19 21:53:58 +0100

    [sage] use unlock_paying in unlocking asset transfer too

--- sage/pallet/src/pallet_impls/unlocks.rs
+++ sage/pallet/src/pallet_impls/unlocks.rs
@@ -15,9 +15,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::{
-	pallet::{PlayerStatsOf, SeasonConfigOf},
-	AccountIdOf, Config, Error, Event, LockableFeature, Pallet, PlayerSeasonConfigs,
-	PlayerSeasonStats, SeasonIdOf, SeasonUnlocks, UnlockRule, UnlockTarget,
+	pallet::PlayerStatsOf, AccountIdOf, Config, Error, Event, LockableFeature, Pallet,
+	PlayerSeasonConfigs, PlayerSeasonStats, SeasonIdOf, SeasonUnlocks, UnlockRule, UnlockTarget,
 };
 use ajuna_primitives::{fee_handler::FeeHandler, season_manager::SeasonManager};
 use frame_support::pallet_prelude::*;
@@ -105,46 +104,14 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 					Err(Error::<T, I>::FeatureLockedInSeason.into())
 				}
 			},
-			UnlockTarget::OneselfPaying =>
-				PlayerSeasonConfigs::<T, I>::try_mutate(&account, &season_id, |config| {
-					if !config.locks.asset_transfer {
-						let SeasonConfigOf::<T, I> { fee, .. } =
-							T::SeasonHandler::get_season_config_for(&season_id)?;
-						T::FeeHandler::deposit_fee_into_treasury(
-							&account,
-							&season_id,
-							fee.unlock_transfer_asset,
-						)?;
-						config.locks.asset_transfer = true;
-
-						Self::deposit_event(Event::FeatureUnlocked {
-							feature: LockableFeature::TransferAsset,
-							season_id: season_id.clone(),
-							account: account.clone(),
-						});
-					}
-					Ok(())
-				}),
+			UnlockTarget::OneselfPaying => Self::unlock_paying(
+				account.clone(),
+				account,
+				season_id,
+				LockableFeature::TransferAsset,
+			),
 			UnlockTarget::OtherPaying(other) =>
-				PlayerSeasonConfigs::<T, I>::try_mutate(&other, &season_id, |config| {
-					if !config.locks.asset_transfer {
-						let SeasonConfigOf::<T, I> { fee, .. } =
-							T::SeasonHandler::get_season_config_for(&season_id)?;
-						T::FeeHandler::deposit_fee_into_treasury(
-							&account,
-							&season_id,
-							fee.unlock_transfer_asset,
-						)?;
-						config.locks.asset_transfer = true;
-
-						Self::deposit_event(Event::FeatureUnlocked {
-							feature: LockableFeature::TransferAsset,
-							season_id: season_id.clone(),
-							account: other.clone(),
-						});
-					}
-					Ok(())
-				}),
+				Self::unlock_paying(account, other, season_id, LockableFeature::TransferAsset),
 		}
 	}
 
