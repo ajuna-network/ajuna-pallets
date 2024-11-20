5cc5c4d69dc1c8e0620475563592c68a3b5bea12
clangenb <37865735+clangenb@users.noreply.github.com>
2024-11-20 09:04:12 +0100

    Improve code for feature unlocking in sage-pallet (#88)

--- sage/pallet/src/pallet_impls/unlocks.rs
+++ sage/pallet/src/pallet_impls/unlocks.rs
@@ -15,9 +15,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::{
-	pallet::{PlayerStatsOf, SeasonConfigOf},
-	AccountIdOf, Config, Error, Event, LockableFeature, Pallet, PlayerSeasonConfigs,
-	PlayerSeasonStats, SeasonIdOf, SeasonUnlocks, UnlockRule, UnlockTarget,
+	pallet::PlayerStatsOf, AccountIdOf, Config, Error, Event, LockableFeature, Pallet,
+	PlayerSeasonConfigs, PlayerSeasonStats, SeasonIdOf, SeasonUnlocks, UnlockRule, UnlockTarget,
 };
 use ajuna_primitives::{fee_handler::FeeHandler, season_manager::SeasonManager};
 use frame_support::pallet_prelude::*;
@@ -28,78 +27,7 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		target: UnlockTarget<AccountIdOf<T>>,
 		season_id: SeasonIdOf<T, I>,
 	) -> DispatchResult {
-		match target {
-			UnlockTarget::OneselfFree => {
-				if let Some(unlock_config) =
-					SeasonUnlocks::<T, I>::get(&season_id, LockableFeature::TradeAsset)
-				{
-					let player_stats = PlayerSeasonStats::<T, I>::get(&account, &season_id);
-
-					PlayerSeasonConfigs::<T, I>::try_mutate(&account, &season_id, |config| {
-						if !config.locks.asset_trade {
-							if Self::evaluate_unlock_state(&unlock_config, &player_stats) {
-								config.locks.asset_trade = true;
-
-								Self::deposit_event(Event::FeatureUnlocked {
-									feature: LockableFeature::TradeAsset,
-									season_id: season_id.clone(),
-									account: account.clone(),
-								});
-
-								Ok(())
-							} else {
-								Err(Error::<T, I>::UnlockCriteriaNotFulfilled.into())
-							}
-						} else {
-							Ok(())
-						}
-					})
-				} else {
-					// TODO: Is this naming correct?
-					Err(Error::<T, I>::FeatureLockedInSeason.into())
-				}
-			},
-			UnlockTarget::OneselfPaying =>
-				PlayerSeasonConfigs::<T, I>::try_mutate(&account, &season_id, |config| {
-					if !config.locks.asset_trade {
-						let SeasonConfigOf::<T, I> { fee, .. } =
-							T::SeasonHandler::get_season_config_for(&season_id)?;
-						T::FeeHandler::deposit_fee_into_treasury(
-							&account,
-							&season_id,
-							fee.unlock_trade_asset,
-						)?;
-						config.locks.asset_trade = true;
-
-						Self::deposit_event(Event::FeatureUnlocked {
-							feature: LockableFeature::TradeAsset,
-							season_id: season_id.clone(),
-							account: account.clone(),
-						});
-					}
-					Ok(())
-				}),
-			UnlockTarget::OtherPaying(other) =>
-				PlayerSeasonConfigs::<T, I>::try_mutate(&other, &season_id, |config| {
-					if !config.locks.asset_trade {
-						let SeasonConfigOf::<T, I> { fee, .. } =
-							T::SeasonHandler::get_season_config_for(&season_id)?;
-						T::FeeHandler::deposit_fee_into_treasury(
-							&account,
-							&season_id,
-							fee.unlock_trade_asset,
-						)?;
-						config.locks.asset_trade = true;
-
-						Self::deposit_event(Event::FeatureUnlocked {
-							feature: LockableFeature::TradeAsset,
-							season_id: season_id.clone(),
-							account: other.clone(),
-						});
-					}
-					Ok(())
-				}),
-		}
+		Self::unlock(account, target, season_id, LockableFeature::TradeAsset)
 	}
 
 	pub(crate) fn unlock_asset_transfer_for(
@@ -107,78 +35,79 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		target: UnlockTarget<AccountIdOf<T>>,
 		season_id: SeasonIdOf<T, I>,
 	) -> DispatchResult {
+		Self::unlock(account, target, season_id, LockableFeature::TransferAsset)
+	}
+
+	fn unlock(
+		account: AccountIdOf<T>,
+		target: UnlockTarget<AccountIdOf<T>>,
+		season_id: SeasonIdOf<T, I>,
+		feature: LockableFeature,
+	) -> DispatchResult {
 		match target {
-			UnlockTarget::OneselfFree => {
-				if let Some(unlock_config) =
-					SeasonUnlocks::<T, I>::get(&season_id, LockableFeature::TransferAsset)
-				{
-					let player_stats = PlayerSeasonStats::<T, I>::get(&account, &season_id);
-
-					PlayerSeasonConfigs::<T, I>::try_mutate(&account, &season_id, |config| {
-						if !config.locks.asset_transfer {
-							if Self::evaluate_unlock_state(&unlock_config, &player_stats) {
-								config.locks.asset_transfer = true;
-
-								Self::deposit_event(Event::FeatureUnlocked {
-									feature: LockableFeature::TransferAsset,
-									season_id: season_id.clone(),
-									account: account.clone(),
-								});
-
-								Ok(())
-							} else {
-								Err(Error::<T, I>::UnlockCriteriaNotFulfilled.into())
-							}
-						} else {
-							Ok(())
-						}
-					})
-				} else {
-					// TODO: Is this naming correct?
-					Err(Error::<T, I>::FeatureLockedInSeason.into())
-				}
-			},
+			UnlockTarget::OneselfFree => Self::unlock_free(account, season_id, feature),
 			UnlockTarget::OneselfPaying =>
-				PlayerSeasonConfigs::<T, I>::try_mutate(&account, &season_id, |config| {
-					if !config.locks.asset_transfer {
-						let SeasonConfigOf::<T, I> { fee, .. } =
-							T::SeasonHandler::get_season_config_for(&season_id)?;
-						T::FeeHandler::deposit_fee_into_treasury(
-							&account,
-							&season_id,
-							fee.unlock_transfer_asset,
-						)?;
-						config.locks.asset_transfer = true;
-
-						Self::deposit_event(Event::FeatureUnlocked {
-							feature: LockableFeature::TransferAsset,
-							season_id: season_id.clone(),
-							account: account.clone(),
-						});
-					}
-					Ok(())
-				}),
+				Self::unlock_paying(account.clone(), account, season_id, feature),
 			UnlockTarget::OtherPaying(other) =>
-				PlayerSeasonConfigs::<T, I>::try_mutate(&other, &season_id, |config| {
-					if !config.locks.asset_transfer {
-						let SeasonConfigOf::<T, I> { fee, .. } =
-							T::SeasonHandler::get_season_config_for(&season_id)?;
-						T::FeeHandler::deposit_fee_into_treasury(
-							&account,
-							&season_id,
-							fee.unlock_transfer_asset,
-						)?;
-						config.locks.asset_transfer = true;
-
-						Self::deposit_event(Event::FeatureUnlocked {
-							feature: LockableFeature::TransferAsset,
-							season_id: season_id.clone(),
-							account: other.clone(),
-						});
-					}
-					Ok(())
-				}),
+				Self::unlock_paying(account, other, season_id, feature),
 		}
+	}
+
+	fn unlock_free(
+		account: AccountIdOf<T>,
+		season_id: SeasonIdOf<T, I>,
+		feature: LockableFeature,
+	) -> DispatchResult {
+		let unlock_config = SeasonUnlocks::<T, I>::get(&season_id, feature)
+			.ok_or(Error::<T, I>::FeatureUnavailableInSeason)?;
+
+		let player_stats = PlayerSeasonStats::<T, I>::get(&account, &season_id);
+
+		if !Self::evaluate_unlock_state(&unlock_config, &player_stats) {
+			return Err(Error::<T, I>::UnlockCriteriaNotFulfilled.into())
+		}
+
+		// after evaluating, we can enable the feature
+		Self::enable_feature_in_config(&account, &season_id, feature);
+
+		Self::deposit_event(Event::FeatureUnlocked { feature, season_id, account });
+		Ok(())
+	}
+
+	fn unlock_paying(
+		payer: AccountIdOf<T>,
+		target: AccountIdOf<T>,
+		season_id: SeasonIdOf<T, I>,
+		feature: LockableFeature,
+	) -> DispatchResult {
+		// first we pay
+		let fee = T::SeasonHandler::get_season_config_for(&season_id)?.fee;
+		let feature_fee = match feature {
+			LockableFeature::TradeAsset => fee.unlock_trade_asset,
+			LockableFeature::TransferAsset => fee.unlock_transfer_asset,
+		};
+		T::FeeHandler::deposit_fee_into_treasury(&payer, &season_id, feature_fee)?;
+
+		// after payment, we can enable the feature
+		Self::enable_feature_in_config(&target, &season_id, feature);
+
+		Self::deposit_event(Event::FeatureUnlocked { feature, season_id, account: target });
+		Ok(())
+	}
+
+	fn enable_feature_in_config(
+		account: &AccountIdOf<T>,
+		season_id: &SeasonIdOf<T, I>,
+		feature: LockableFeature,
+	) {
+		PlayerSeasonConfigs::<T, I>::mutate(account, season_id, |config| {
+			let feature_lock = match feature {
+				LockableFeature::TradeAsset => &mut config.locks.asset_trade,
+				LockableFeature::TransferAsset => &mut config.locks.asset_transfer,
+			};
+
+			*feature_lock = true;
+		});
 	}
 
 	fn evaluate_unlock_state(config: &UnlockRule, account_stats: &PlayerStatsOf<T>) -> bool {
