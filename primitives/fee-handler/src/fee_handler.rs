4d76803b972f81ae014a7e8a2e13c203b0e4bf09
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-12-01 21:37:54 +0100

    get closer to final payment handler

--- primitives/fee-handler/src/fee_handler.rs
+++ primitives/fee-handler/src/fee_handler.rs
@@ -1,13 +1,9 @@
 use ajuna_primitives::treasury_manager::TreasuryManager;
 use core::marker::PhantomData;
 use frame_support::{
-	pallet_prelude::{DispatchError, InvalidTransaction},
-	sp_runtime::{traits::CheckedSub, ArithmeticError},
-	traits::{
-		fungibles, fungibles::Mutate, tokens::Preservation, Currency,
-		ExistenceRequirement::KeepAlive, Get,
-	},
-	Parameter,
+	pallet_prelude::DispatchError,
+	sp_runtime::{traits::CheckedSub, TokenError},
+	traits::{fungibles, fungibles::Balanced, tokens::Precision, Get},
 };
 use pallet_asset_conversion::Pallet as AssetConversion;
 
@@ -89,12 +85,14 @@ pub trait FeeHandler {
 	type TreasuryKey;
 
 	fn try_propagate_chain_fee(
+		payment_asset: Self::AssetId,
 		base_fee: Self::FeeBalance,
 		account: &Self::AccountId,
 		identifier: &Self::AffiliateFeeIdentifier,
 	) -> Result<Self::FeeBalance, DispatchError>;
 
 	fn try_propagate_tournament_fee(
+		payment_asset: Self::AssetId,
 		base_fee: Self::FeeBalance,
 		account: &Self::AccountId,
 		identifier: &Self::TournamentFeeIdentifier,
@@ -111,10 +109,10 @@ pub struct GameFeeHandler<AssetConversion, PaymentAsset, Affiliate, Tournament, 
 	_phantom: PhantomData<(AssetConversion, PaymentAsset, Affiliate, Tournament, Treasury)>,
 }
 
-impl<T, PaymentAsset, Affiliate, Tournament, Treasury> FeeHandler
-	for GameFeeHandler<T, PaymentAsset, Affiliate, Tournament, Treasury>
+impl<T, DepositAsset, Affiliate, Tournament, Treasury> FeeHandler
+	for GameFeeHandler<T, DepositAsset, Affiliate, Tournament, Treasury>
 where
-	PaymentAsset: Get<T::AssetKind>,
+	DepositAsset: Get<T::AssetKind>,
 	T: pallet_asset_conversion::Config,
 	T::Assets: fungibles::Inspect<T::AccountId, Balance = T::Balance, AssetId = T::AssetKind>,
 
@@ -137,53 +135,75 @@ where
 	type TournamentFeeIdentifier = Tournament::FeeIdentifier;
 	type TreasuryKey = Treasury::TreasuryPotKey;
 
+	/// Distributes an already withdrawn `fee_credit` to the affiliates of `account`.
+	///
+	/// Returns the remaining `fee_credit` after this operation.
 	fn try_propagate_chain_fee(
-		base_fee: Self::FeeBalance,
+		deposit_asset: Self::AssetId,
+		fee_credit: Self::FeeBalance,
 		account: &Self::AccountId,
 		identifier: &Self::AffiliateFeeIdentifier,
 	) -> Result<Self::FeeBalance, DispatchError> {
-		let mut final_fee = base_fee;
+		let mut final_fee = fee_credit;
 
-		for (transfer_fee, chain_account) in Affiliate::get_fee_from(base_fee, account, identifier)
+		for (transfer_fee, chain_account) in
+			Affiliate::get_fee_from(fee_credit, account, identifier)
 		{
 			if transfer_fee > 0_u32.into() {
-				T::Assets::transfer(
-					PaymentAsset::get(),
-					account,
-					&chain_account,
-					transfer_fee,
-					Preservation::Preserve,
-				)?;
 				final_fee = final_fee
 					.checked_sub(&transfer_fee)
-					.ok_or(DispatchError::Arithmetic(ArithmeticError::Underflow))?;
+					.ok_or(DispatchError::Token(TokenError::FundsUnavailable))?;
+
+				let debt = T::Assets::deposit(
+					deposit_asset.clone(),
+					&chain_account,
+					transfer_fee.clone(),
+					Precision::BestEffort,
+				)?;
+
+				if debt.peek() != transfer_fee {
+					// we should never reach this arm, but it is better to double-check.
+					return Err(DispatchError::Other("Unexpected error in fee payment."));
+				}
 			}
 		}
 
 		Ok(final_fee)
 	}
 
+	/// Deposits the tournament fee into the tournament pot. The fee is taken from a previously
+	/// withdrawn `fee_credit`.
+	///
+	/// Returns the remaining credit after taking the fee.
 	fn try_propagate_tournament_fee(
-		base_fee: Self::FeeBalance,
+		deposit_asset: Self::AssetId,
+		fee_credit: Self::FeeBalance,
 		account: &Self::AccountId,
 		identifier: &Self::TournamentFeeIdentifier,
 	) -> Result<Self::FeeBalance, DispatchError> {
 		let (tournament_fee, tournament_account) =
-			Tournament::get_fee_from(base_fee, account, identifier);
+			Tournament::get_fee_from(fee_credit, account, identifier);
 
 		if tournament_fee > 0_u32.into() {
-			T::Assets::transfer(
-				PaymentAsset::get(),
-				account,
-				&tournament_account,
-				tournament_fee,
-				Preservation::Preserve,
-			)?;
-			base_fee
+			let remaining_credit = fee_credit
 				.checked_sub(&tournament_fee)
-				.ok_or(DispatchError::Arithmetic(ArithmeticError::Underflow))
+				.ok_or(DispatchError::Token(TokenError::FundsUnavailable))?;
+
+			let debt = T::Assets::deposit(
+				deposit_asset,
+				&tournament_account,
+				tournament_fee.clone(),
+				Precision::BestEffort,
+			)?;
+
+			if debt.peek() == tournament_fee {
+				Ok(remaining_credit)
+			} else {
+				// we should never reach this arm, but it is better to double-check.
+				Err(DispatchError::Other("Unexpected error in fee payment."))
+			}
 		} else {
-			Ok(base_fee)
+			Ok(fee_credit)
 		}
 	}
 
