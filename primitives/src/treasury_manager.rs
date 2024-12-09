616abe3a72ecce96dccf7d60ab2b614d2d0bc26b
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-12-09 13:22:44 +0100

    fmt

--- primitives/src/treasury_manager.rs
+++ primitives/src/treasury_manager.rs
@@ -37,4 +37,4 @@ pub trait TreasuryManager {
 
 	/// Get the assigned holder of the treasury pot for the given key
 	fn get_treasurer_for(key: Self::TreasuryPotKey) -> Result<Self::AccountId, DispatchError>;
-	}
+}
