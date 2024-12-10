ff2867e6cea621bcc10d822f793aa9c519f9f5e4
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-12-10 17:03:27 +0100

    fix recursive search for cargo toml

--- scripts/run_for_all_crates.sh
+++ scripts/run_for_all_crates.sh
@@ -9,12 +9,15 @@ shift
 
 set -x
 
-for file in **/Cargo.toml; do
-  if grep -q "\[features\]" "$file" && grep -q "runtime-benchmarks" "$file"; then
+find . -name "Cargo.toml" | while read -r CARGO_TOML; do
+  DIR=$(dirname "$CARGO_TOML")
+  echo "Checking in directory: $DIR"
+
+  if grep -q "\[features\]" "$CARGO_TOML" && grep -q "runtime-benchmarks" "$CARGO_TOML"; then
       echo "Feature 'runtime-benchmarks' found, adding this feature."
-      cargo $COMMAND $@ --features runtime-benchmarks --manifest-path "$file"
+      cargo $COMMAND $@ --features runtime-benchmarks --manifest-path "$CARGO_TOML"
   else
       echo "Feature 'runtime-benchmarks' not found, running command without this feature"
-      cargo $COMMAND $@ --manifest-path "$file";
+      cargo $COMMAND $@ --manifest-path "$CARGO_TOML";
   fi
 done
