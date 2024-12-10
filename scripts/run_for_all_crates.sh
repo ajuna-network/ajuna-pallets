234bd794e1d19fe4795ff58987ef78c76d65d934
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-12-10 16:55:11 +0100

    conditionally add runtime-features benchmark

--- scripts/run_for_all_crates.sh
+++ scripts/run_for_all_crates.sh
@@ -10,5 +10,11 @@ shift
 set -x
 
 for file in **/Cargo.toml; do
-	cargo $COMMAND $@ --manifest-path "$file";
+  if grep -q "\[features\]" "$file" && grep -q "runtime-benchmarks" "$file"; then
+      echo "Feature 'runtime-benchmarks' found, adding this feature."
+      cargo $COMMAND $@ --features runtime-benchmarks --manifest-path "$file"
+  else
+      echo "Feature 'runtime-benchmarks' not found, running command without this feature"
+      cargo $COMMAND $@ --manifest-path "$file";
+  fi
 done
