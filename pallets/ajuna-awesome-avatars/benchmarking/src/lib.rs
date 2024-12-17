99d594977f6a435a96f505525ee3caebc61a2ee3
Dídac Sementé Fernández <didac@ajuna.io>
2024-12-17 17:59:44 +0100

    * Converted AAA benchmarks to V2

--- pallets/ajuna-awesome-avatars/benchmarking/src/lib.rs
+++ pallets/ajuna-awesome-avatars/benchmarking/src/lib.rs
@@ -20,7 +20,7 @@
 
 mod mock;
 
-use frame_benchmarking::benchmarks;
+use frame_benchmarking::v2::*;
 use frame_support::traits::{Currency, Get};
 use frame_system::{pallet_prelude::BlockNumberFor, RawOrigin};
 use pallet_ajuna_awesome_avatars::{
@@ -49,107 +49,132 @@ fn assert_last_event<T: Config>(avatars_event: Event<T>) {
 	frame_system::Pallet::<T>::assert_last_event(event.into());
 }
 
-benchmarks! {
-	mint_free {
+#[benchmarks]
+mod benchmarks {
+	use super::*;
+
+	#[benchmark]
+	fn mint_free(n: Linear<0, { MaxAvatarsPerPlayer::get() - 6 }>) {
 		let name = "player";
 		let caller = account::<T>(name);
-		let n in 0 .. (MaxAvatarsPerPlayer::get() - 6);
-		create_avatars::<T>(caller.clone(), n)?;
+		create_avatars::<T>(caller.clone(), n).expect("Should create avatars");
 
 		PlayerConfigs::<T>::mutate(&caller, |account| account.free_mints = MintCount::MAX);
 
-		let mint_option = MintOption { payment: MintPayment::Free, pack_size: MintPackSize::Six,
-			pack_type: PackType::Material, };
-	}: mint(RawOrigin::Signed(caller.clone()), mint_option)
-	verify {
-		let n = n as usize;
+		let mint_option = MintOption {
+			payment: MintPayment::Free,
+			pack_size: MintPackSize::Six,
+			pack_type: PackType::Material,
+		};
+
+		#[extrinsic_call]
+		mint(RawOrigin::Signed(caller.clone()), mint_option);
+
+		let i = n as usize;
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
-		let avatar_ids = Owners::<T>::get(caller, season_id)[n..(n + 6)].to_vec();
-		assert_last_event::<T>(Event::AvatarsMinted { avatar_ids })
+		let avatar_ids = Owners::<T>::get(caller, season_id)[i..(i + 6)].to_vec();
+		assert_last_event::<T>(Event::AvatarsMinted { avatar_ids });
 	}
 
-	mint_normal {
+	#[benchmark]
+	fn mint_normal(n: Linear<0, { MaxAvatarsPerPlayer::get() - 6 }>) {
 		let name = "player";
 		let caller = account::<T>(name);
 
-		let n in 0 .. (MaxAvatarsPerPlayer::get() - 6);
-		create_avatars::<T>(caller.clone(), n)?;
+		create_avatars::<T>(caller.clone(), n).expect("Should create avatars");
 
 		let season = Seasons::<T>::get(CurrentSeasonStatus::<T>::get().season_id).unwrap();
 		let mint_fee = season.fee.mint.fee_for(&MintPackSize::Six);
 		let ed = CurrencyOf::<T>::minimum_balance();
 		CurrencyOf::<T>::make_free_balance_be(&caller, mint_fee + ed);
 
-		let mint_option = MintOption { payment: MintPayment::Normal, pack_size: MintPackSize::Six,
-			pack_type: PackType::Material };
-	}: mint(RawOrigin::Signed(caller.clone()), mint_option)
-	verify {
-		let n = n as usize;
+		let mint_option = MintOption {
+			payment: MintPayment::Normal,
+			pack_size: MintPackSize::Six,
+			pack_type: PackType::Material,
+		};
+
+		#[extrinsic_call]
+		mint(RawOrigin::Signed(caller.clone()), mint_option);
+
+		let i = n as usize;
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
-		let avatar_ids = Owners::<T>::get(caller, season_id)[n..(n + 6)].to_vec();
-		assert_last_event::<T>(Event::AvatarsMinted { avatar_ids })
+		let avatar_ids = Owners::<T>::get(caller, season_id)[i..(i + 6)].to_vec();
+		assert_last_event::<T>(Event::AvatarsMinted { avatar_ids });
 	}
 
-	forge {
+	#[benchmark]
+	fn forge(n: Linear<5, { MaxAvatarsPerPlayer::get() - 10 }>) {
 		let name = "player";
 		let player = account::<T>(name);
-		let n in 5 .. (MaxAvatarsPerPlayer::get() - 10);
-		create_avatars::<T>(player.clone(), n)?;
+		create_avatars::<T>(player.clone(), n).expect("Should create avatars");
 
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_ids = Owners::<T>::get(&player, season_id);
 		let avatar_id = avatar_ids[0];
-		let (_owner, original_avatar) = Avatars::<T>::get(avatar_id).unwrap();
-	}: _(RawOrigin::Signed(player), avatar_id, avatar_ids[1..5].to_vec())
-	verify {
-		let (_owner, upgraded_avatar) = Avatars::<T>::get(avatar_id).unwrap();
+		let (_, original_avatar) = Avatars::<T>::get(avatar_id).unwrap();
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(player), avatar_id, avatar_ids[1..5].to_vec());
+
+		let (_, upgraded_avatar) = Avatars::<T>::get(avatar_id).unwrap();
 		let original_tiers = original_avatar.dna.into_iter().map(|x| x >> 4);
 		let upgraded_tiers = upgraded_avatar.dna.into_iter().map(|x| x >> 4);
-		let upgraded_components = original_tiers.zip(upgraded_tiers).fold(
-			0, |mut count, (lhs, rhs)| {
+		let upgraded_components =
+			original_tiers.zip(upgraded_tiers).fold(0, |mut count, (lhs, rhs)| {
 				if lhs != rhs {
-					count+=1;
+					count += 1;
 				}
 				count
-			}
-		);
-		assert_last_event::<T>(Event::AvatarsForged { avatar_ids: vec![(avatar_id, upgraded_components)] })
+			});
+		assert_last_event::<T>(Event::AvatarsForged {
+			avatar_ids: vec![(avatar_id, upgraded_components)],
+		});
 	}
 
-	transfer_avatar_normal {
+	#[benchmark]
+	fn transfer_avatar_normal(n: Linear<1, { MaxAvatarsPerPlayer::get() }>) {
 		let from = account::<T>("from");
 		let to = account::<T>("to");
-		let n in 1 .. MaxAvatarsPerPlayer::get();
-		create_avatars::<T>(from.clone(), MaxAvatarsPerPlayer::get())?;
-		create_avatars::<T>(to.clone(), MaxAvatarsPerPlayer::get() - n)?;
+		create_avatars::<T>(from.clone(), MaxAvatarsPerPlayer::get())
+			.expect("Should create avatars");
+		create_avatars::<T>(to.clone(), MaxAvatarsPerPlayer::get() - n)
+			.expect("Should create avatars");
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_id = Owners::<T>::get(&from, season_id)[n as usize - 1];
 
 		let Season { fee, .. } = Seasons::<T>::get(season_id).unwrap();
 		CurrencyOf::<T>::make_free_balance_be(&from, fee.transfer_avatar);
-	}: transfer_avatar(RawOrigin::Signed(from.clone()), to.clone(), avatar_id)
-	verify {
-		assert_last_event::<T>(Event::AvatarTransferred { from, to, avatar_id })
+
+		#[extrinsic_call]
+		transfer_avatar(RawOrigin::Signed(from.clone()), to.clone(), avatar_id);
+
+		assert_last_event::<T>(Event::AvatarTransferred { from, to, avatar_id });
 	}
 
-	transfer_avatar_organizer {
+	#[benchmark]
+	fn transfer_avatar_organizer(n: Linear<1, { MaxAvatarsPerPlayer::get() }>) {
 		let organizer = account::<T>("organizer");
 		let to = account::<T>("to");
-		let n in 1 .. MaxAvatarsPerPlayer::get();
-		create_avatars::<T>(organizer.clone(), MaxAvatarsPerPlayer::get())?;
-		create_avatars::<T>(to.clone(), MaxAvatarsPerPlayer::get() - n)?;
+		create_avatars::<T>(organizer.clone(), MaxAvatarsPerPlayer::get())
+			.expect("Should create avatars");
+		create_avatars::<T>(to.clone(), MaxAvatarsPerPlayer::get() - n)
+			.expect("Should create avatars");
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_id = Owners::<T>::get(&organizer, season_id)[n as usize - 1];
 
 		let Season { fee, .. } = Seasons::<T>::get(season_id).unwrap();
 		CurrencyOf::<T>::make_free_balance_be(&organizer, fee.transfer_avatar);
-	}: transfer_avatar(RawOrigin::Signed(organizer.clone()), to.clone(), avatar_id)
-	verify {
-		assert_last_event::<T>(Event::AvatarTransferred { from: organizer, to, avatar_id })
+
+		#[extrinsic_call]
+		transfer_avatar(RawOrigin::Signed(organizer.clone()), to.clone(), avatar_id);
+
+		assert_last_event::<T>(Event::AvatarTransferred { from: organizer, to, avatar_id });
 	}
 
-	transfer_free_mints {
-		create_seasons::<T>(1)?;
+	#[benchmark]
+	fn transfer_free_mints() {
+		create_seasons::<T>(1).expect("Should create seasons");
 		let from = account::<T>("from");
 		let to = account::<T>("to");
 		let GlobalConfig { freemint_transfer, .. } = GlobalConfigs::<T>::get();
@@ -161,41 +186,51 @@ benchmarks! {
 			stats.forged = 1;
 		});
 		PlayerConfigs::<T>::mutate(&from, |account| account.free_mints = MintCount::MAX);
-	}: _(RawOrigin::Signed(from.clone()), to.clone(), how_many)
-	verify {
-		assert_last_event::<T>(Event::FreeMintsTransferred { from, to, how_many })
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(from.clone()), to.clone(), how_many);
+
+		assert_last_event::<T>(Event::FreeMintsTransferred { from, to, how_many });
 	}
 
-	set_price {
+	#[benchmark]
+	fn set_price() {
 		let name = "player";
 		let caller = account::<T>(name);
-		create_avatars::<T>(caller.clone(), MaxAvatarsPerPlayer::get())?;
+		create_avatars::<T>(caller.clone(), MaxAvatarsPerPlayer::get())
+			.expect("Should create avatars");
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_id = Owners::<T>::get(&caller, season_id)[0];
 		let price = BalanceOf::<T>::unique_saturated_from(u128::MAX);
-	}: _(RawOrigin::Signed(caller), avatar_id, price)
-	verify {
-		assert_last_event::<T>(Event::AvatarPriceSet { avatar_id, price })
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(caller), avatar_id, price);
+
+		assert_last_event::<T>(Event::AvatarPriceSet { avatar_id, price });
 	}
 
-	remove_price {
+	#[benchmark]
+	fn remove_price() {
 		let name = "player";
 		let caller = account::<T>(name);
-		create_avatars::<T>(caller.clone(), MaxAvatarsPerPlayer::get())?;
+		create_avatars::<T>(caller.clone(), MaxAvatarsPerPlayer::get())
+			.expect("Should create avatars");
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_id = Owners::<T>::get(&caller, season_id)[0];
 		Trade::<T>::insert(season_id, avatar_id, BalanceOf::<T>::unique_saturated_from(u128::MAX));
-	}: _(RawOrigin::Signed(caller), avatar_id)
-	verify {
-		assert_last_event::<T>(Event::AvatarPriceUnset { avatar_id })
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(caller), avatar_id);
+
+		assert_last_event::<T>(Event::AvatarPriceUnset { avatar_id });
 	}
 
-	buy {
+	#[benchmark]
+	fn buy(n: Linear<1, { MaxAvatarsPerPlayer::get() }>) {
 		let (buyer_name, seller_name) = ("buyer", "seller");
 		let (buyer, seller) = (account::<T>(buyer_name), account::<T>(seller_name));
-		let n in 1 .. MaxAvatarsPerPlayer::get();
-		create_avatars::<T>(buyer.clone(), n - 1)?;
-		create_avatars::<T>(seller.clone(), n)?;
+		create_avatars::<T>(buyer.clone(), n - 1).expect("Should create avatars");
+		create_avatars::<T>(seller.clone(), n).expect("Should create avatars");
 
 		let ed = CurrencyOf::<T>::minimum_balance();
 		let current_season_id = CurrentSeasonStatus::<T>::get().season_id;
@@ -211,42 +246,60 @@ benchmarks! {
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_id = Owners::<T>::get(&seller, season_id)[0];
 		Trade::<T>::insert(season_id, avatar_id, sell_fee);
-	}: _(RawOrigin::Signed(buyer.clone()), avatar_id)
-	verify {
-		assert_last_event::<T>(Event::AvatarTraded { avatar_id, from: seller, to: buyer, price: sell_fee })
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(buyer.clone()), avatar_id);
+
+		assert_last_event::<T>(Event::AvatarTraded {
+			avatar_id,
+			from: seller,
+			to: buyer,
+			price: sell_fee,
+		});
 	}
 
-	upgrade_storage {
-		create_seasons::<T>(1)?;
+	#[benchmark]
+	fn upgrade_storage() {
+		create_seasons::<T>(1).expect("Should create seasons");
 		let player = account::<T>("player");
 		let current_season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let season = Seasons::<T>::get(current_season_id).unwrap();
 		let ed = CurrencyOf::<T>::minimum_balance();
 		CurrencyOf::<T>::make_free_balance_be(&player, season.fee.upgrade_storage + ed);
-	}: _(RawOrigin::Signed(player.clone()), None, None)
-	verify {
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(player.clone()), None, None);
+
 		assert_last_event::<T>(Event::StorageTierUpgraded {
-			account: player, season_id: current_season_id,
-		})
+			account: player,
+			season_id: current_season_id,
+		});
 	}
 
-	set_organizer {
+	#[benchmark]
+	fn set_organizer() {
 		let organizer = account::<T>("organizer");
-	}: _(RawOrigin::Root, organizer.clone())
-	verify {
-		assert_last_event::<T>(Event::<T>::OrganizerSet { organizer })
+
+		#[extrinsic_call]
+		_(RawOrigin::Root, organizer.clone());
+
+		assert_last_event::<T>(Event::<T>::OrganizerSet { organizer });
 	}
 
-	set_treasurer {
+	#[benchmark]
+	fn set_treasurer() {
 		let season_id = 369;
 		let treasurer = account::<T>("treasurer");
-	}: _(RawOrigin::Root, season_id, treasurer.clone())
-	verify {
-		assert_last_event::<T>(Event::TreasurerSet { season_id, treasurer })
+
+		#[extrinsic_call]
+		_(RawOrigin::Root, season_id, treasurer.clone());
+
+		assert_last_event::<T>(Event::TreasurerSet { season_id, treasurer });
 	}
 
-	claim_treasury {
-		create_seasons::<T>(3)?;
+	#[benchmark]
+	fn claim_treasury() {
+		create_seasons::<T>(3).expect("Should create seasons");
 		let season_id = 1;
 		let treasurer = account::<T>("treasurer");
 		let amount = 1_000_000_000_000_u64.unique_saturated_into();
@@ -254,12 +307,15 @@ benchmarks! {
 		Treasury::<T>::mutate(season_id, |balance| balance.saturating_accrue(amount));
 		CurrencyOf::<T>::deposit_creating(&AAvatars::<T>::treasury_account_id(), amount);
 		CurrencyOf::<T>::make_free_balance_be(&treasurer, CurrencyOf::<T>::minimum_balance());
-	}: _(RawOrigin::Signed(treasurer.clone()), season_id)
-	verify {
-		assert_last_event::<T>(Event::TreasuryClaimed { season_id, treasurer, amount })
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(treasurer.clone()), season_id);
+
+		assert_last_event::<T>(Event::TreasuryClaimed { season_id, treasurer, amount });
 	}
 
-	set_season {
+	#[benchmark]
+	fn set_season() {
 		let organizer = account::<T>("organizer");
 		Organizer::<T>::put(&organizer);
 
@@ -311,23 +367,41 @@ benchmarks! {
 			start: BlockNumberFor::<T>::from(u32::MAX - 1),
 			end: BlockNumberFor::<T>::from(u32::MAX),
 		};
-		let trade_filters = TradeFilters(sp_runtime::BoundedVec::try_from(vec![
-			u32::from_le_bytes([0x11, 0x07, 0x00, 0x00]), // CrazyDude pet
-			u32::from_le_bytes([0x12, 0x36, 0x00, 0x00]), // GiantWoodStick armor front pet part
-			u32::from_le_bytes([0x25, 0x07, 0x00, 0xFF]), // Metals of quantity 255
-			u32::from_le_bytes([0x25, 0x02, 0x00, 0x00]), // Electronics of any quantity
-			u32::from_le_bytes([0x30, 0x00, 0x00, 0x00]), // Any Essence
-			u32::from_le_bytes([0x41, 0x00, 0x00, 0xF0]), // ArmorBase of quantity 240
-			u32::from_le_bytes([0x45, 0x00, 0x00, 0x0F]), // WeaponVersion1 of quantity 15
-		]).expect("Should create vec"));
-	}: _(RawOrigin::Signed(organizer), season_id, Some(season.clone()), Some(season_meta.clone()), Some(season_schedule.clone()), Some(trade_filters.clone()))
-	verify {
+		let trade_filters = TradeFilters(
+			sp_runtime::BoundedVec::try_from(vec![
+				u32::from_le_bytes([0x11, 0x07, 0x00, 0x00]), // CrazyDude pet
+				u32::from_le_bytes([0x12, 0x36, 0x00, 0x00]), /* GiantWoodStick armor front pet
+				                                               * part */
+				u32::from_le_bytes([0x25, 0x07, 0x00, 0xFF]), // Metals of quantity 255
+				u32::from_le_bytes([0x25, 0x02, 0x00, 0x00]), // Electronics of any quantity
+				u32::from_le_bytes([0x30, 0x00, 0x00, 0x00]), // Any Essence
+				u32::from_le_bytes([0x41, 0x00, 0x00, 0xF0]), // ArmorBase of quantity 240
+				u32::from_le_bytes([0x45, 0x00, 0x00, 0x0F]), // WeaponVersion1 of quantity 15
+			])
+			.expect("Should create vec"),
+		);
+
+		#[extrinsic_call]
+		_(
+			RawOrigin::Signed(organizer),
+			season_id,
+			Some(season.clone()),
+			Some(season_meta.clone()),
+			Some(season_schedule.clone()),
+			Some(trade_filters.clone()),
+		);
+
 		assert_last_event::<T>(Event::UpdatedSeason {
-			season_id, season: Some(season), meta: Some(season_meta), schedule: Some(season_schedule), trade_filters: Some(trade_filters)
-		})
+			season_id,
+			season: Some(season),
+			meta: Some(season_meta),
+			schedule: Some(season_schedule),
+			trade_filters: Some(trade_filters),
+		});
 	}
 
-	update_global_config {
+	#[benchmark]
+	fn update_global_config() {
 		let organizer = account::<T>("organizer");
 		Organizer::<T>::put(&organizer);
 
@@ -338,9 +412,7 @@ benchmarks! {
 				free_mint_fee_multiplier: MintCount::MAX,
 			},
 			forge: ForgeConfig { open: true },
-			avatar_transfer: AvatarTransferConfig {
-				open: true,
-			},
+			avatar_transfer: AvatarTransferConfig { open: true },
 			freemint_transfer: FreemintTransferConfig {
 				mode: FreeMintTransferMode::Open,
 				free_mint_transfer_fee: MintCount::MAX,
@@ -350,59 +422,61 @@ benchmarks! {
 			nft_transfer: NftTransferConfig { open: true },
 			affiliate_config: AffiliateConfig::default(),
 		};
-	}: _(RawOrigin::Signed(organizer), config.clone())
-	verify {
-		assert_last_event::<T>(Event::UpdatedGlobalConfig(config))
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(organizer), config.clone());
+
+		assert_last_event::<T>(Event::UpdatedGlobalConfig(config));
 	}
 
-	set_free_mints {
+	#[benchmark]
+	fn set_free_mints() {
 		let organizer = account::<T>("organizer");
 		Organizer::<T>::put(&organizer);
 
 		let target = account::<T>("target");
 		let how_many = MintCount::MAX;
-	}: _(RawOrigin::Signed(organizer), target.clone(), how_many)
-	verify {
+
+		#[extrinsic_call]
+		_(RawOrigin::Signed(organizer), target.clone(), how_many);
+
 		assert_last_event::<T>(Event::FreeMintsSet { target, how_many });
 	}
 
-	lock_avatar {
+	#[benchmark]
+	fn lock_avatar(n: Linear<1, { MaxAvatarsPerPlayer::get() }>) {
 		let name = "player";
 		let player = account::<T>(name);
-		let n in 1 .. MaxAvatarsPerPlayer::get();
-		create_avatars::<T>(player.clone(), n)?;
+		create_avatars::<T>(player.clone(), n).expect("Should create avatars");
 
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_ids = Owners::<T>::get(&player, season_id);
 		let avatar_id = avatar_ids[avatar_ids.len() - 1];
 
-		let organizer = account::<T>("organizer");
-	}: _(RawOrigin::Signed(player), avatar_id)
-	verify {
-		assert_last_event::<T>(Event::AvatarLocked { avatar_id })
+		#[extrinsic_call]
+		_(RawOrigin::Signed(player), avatar_id);
+
+		assert_last_event::<T>(Event::AvatarLocked { avatar_id });
 	}
 
-	unlock_avatar {
+	#[benchmark]
+	fn unlock_avatar(n: Linear<1, { MaxAvatarsPerPlayer::get() }>) {
 		let name = "player";
 		let player = account::<T>(name);
-		let n in 1 .. MaxAvatarsPerPlayer::get();
-		create_avatars::<T>(player.clone(), n)?;
+		create_avatars::<T>(player.clone(), n).expect("Should create avatars");
 
 		let season_id = CurrentSeasonStatus::<T>::get().season_id;
 		let avatar_ids = Owners::<T>::get(&player, season_id);
 		let avatar_id = avatar_ids[avatar_ids.len() - 1];
 
-		let organizer = account::<T>("organizer");
+		AAvatars::<T>::lock_avatar(RawOrigin::Signed(player.clone()).into(), avatar_id)
+			.expect("Should lock avatar");
 
-		AAvatars::<T>::lock_avatar(RawOrigin::Signed(player.clone()).into(), avatar_id)?;
-	}: _(RawOrigin::Signed(player), avatar_id)
-	verify {
-		assert_last_event::<T>(Event::AvatarUnlocked { avatar_id })
+		#[extrinsic_call]
+		_(RawOrigin::Signed(player), avatar_id);
+
+		assert_last_event::<T>(Event::AvatarUnlocked { avatar_id });
 	}
 
-	impl_benchmark_test_suite!(
-		Pallet,
-		crate::mock::new_test_ext(),
-		crate::mock::Runtime
-	);
+	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Runtime);
 }
