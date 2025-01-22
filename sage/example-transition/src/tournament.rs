1a3dcedf354014193990a5c5d2e7c478b84c4062
Dídac Sementé Fernández <didac@ajuna.io>
2025-01-22 14:45:35 +0100

    * Add entity ranker

--- sage/example-transition/src/tournament.rs
+++ sage/example-transition/src/tournament.rs
@@ -1,3 +1,30 @@
+use crate::asset::{Asset, AssetId};
+
+use ajuna_primitives::tournament_manager::EntityRanker;
+
+use frame_support::pallet_prelude::{Decode, Encode, MaxEncodedLen, TypeInfo};
+use sp_std::{cmp::Ordering, marker::PhantomData};
+
 pub type HeroJamTournamentCategoryId = u8;
 
 pub type HeroJamTournamentConfig = ();
+
+#[derive(Encode, Decode, MaxEncodedLen, TypeInfo, Clone, Debug, PartialEq, Eq)]
+pub struct HeroJamEntityRanker<BlockNumber>(PhantomData<BlockNumber>);
+
+impl<BlockNumber> EntityRanker for HeroJamEntityRanker<BlockNumber> {
+	type EntityId = AssetId;
+	type Entity = Asset<BlockNumber>;
+
+	fn can_rank(&self, _entity: (&Self::EntityId, &Self::Entity)) -> bool {
+		true
+	}
+
+	fn rank_against(
+		&self,
+		_entity: (&Self::EntityId, &Self::Entity),
+		_other: (&Self::EntityId, &Self::Entity),
+	) -> Ordering {
+		Ordering::Equal
+	}
+}
