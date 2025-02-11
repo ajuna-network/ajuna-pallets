47f504fb4a436f85b24655acb990d3ca4d0e1978
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-02-11 10:53:12 +0100

    [sage] derive stuff for the transition error

--- sage/api/src/error.rs
+++ sage/api/src/error.rs
@@ -1,7 +1,9 @@
 use frame_support::pallet_prelude::{Decode, Encode, MaxEncodedLen, TypeInfo};
 
 /// Errors that may happen during the execution of a SAGE transition.
-#[derive(Encode, Decode, MaxEncodedLen, TypeInfo, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(
+	Encode, Decode, MaxEncodedLen, TypeInfo, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord,
+)]
 pub enum TransitionError {
 	TransferError,
 	FeeError,
