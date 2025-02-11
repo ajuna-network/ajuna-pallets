649eb672d78fbae5d722bddd21233ba116c1f712
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-02-11 10:36:53 +0100

    add some more documentation to the transition associated types

--- sage/api/src/traits.rs
+++ sage/api/src/traits.rs
@@ -19,8 +19,11 @@ pub trait SageGameTransition {
 	type TransitionId: Member + Parameter + MaxEncodedLen + TypeInfo;
 	/// THe type of the specific configuration options for the transition
 	type TransitionConfig: Member + Parameter + MaxEncodedLen + TypeInfo + Default;
+	/// The account id type, usually 32 bytes long.
 	type AccountId: Member + Codec;
+	/// The asset id type of the sage assets defined by the developer.
 	type AssetId: Member + Parameter + MaxEncodedLen + TypeInfo;
+	/// The asset type used in sage defined by the game developer.
 	type Asset: Member + Parameter + MaxEncodedLen + TypeInfo + GetId<Self::AssetId>;
 	/// An optional extra, which is simply forwarded to the `verify_rule` and `do_transition`
 	/// method. If you don't need custom arguments, you can define that type as `()`.
@@ -29,6 +32,8 @@ pub trait SageGameTransition {
 	/// Defines the fungible asset that was used to pay the transaction. If the transition accesses
 	/// user funds, it might want to use the same asset, as this implies that the user is willing
 	/// to use this one instead of the native balance.
+	///
+	/// If you don't need fungible tokens in your transition you can set this as ().
 	type PaymentFungible;
 
 	fn do_transition(
