efcbfdb317eb1ede7a421ac13d60acf6a193c663
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-03-07 16:20:35 +0100

    [ajuna-primitives] add a test for a random next asset provider

--- primitives/src/next_asset_id_provider.rs
+++ primitives/src/next_asset_id_provider.rs
@@ -1,6 +1,7 @@
 use frame_support::{sp_runtime::traits::CheckedAdd, traits::Randomness};
 use std::marker::PhantomData;
 use frame_support::sp_runtime::traits::One;
+use parity_scale_codec::Encode;
 
 pub trait ProvideNextAssetId {
 	type AssetId;
@@ -22,13 +23,13 @@ pub struct RandomAssetIdProvider<AssetId, BlockNumber, RandomnessSource>(
 	PhantomData<(AssetId, BlockNumber, RandomnessSource)>,
 );
 
-impl<AssetId: AsRef<[u8]>, BlockNumber, R: Randomness<AssetId, BlockNumber>> ProvideNextAssetId
+impl<AssetId: Encode, BlockNumber, R: Randomness<AssetId, BlockNumber>> ProvideNextAssetId
 	for RandomAssetIdProvider<AssetId, BlockNumber, R>
 {
 	type AssetId = AssetId;
 
 	fn next_asset_id(current_asset_id: &Self::AssetId) -> Option<Self::AssetId> {
-		Some(R::random(current_asset_id.as_ref()).0)
+		Some(R::random(current_asset_id.encode().as_slice()).0)
 	}
 }
 
@@ -36,11 +37,29 @@ impl<AssetId: AsRef<[u8]>, BlockNumber, R: Randomness<AssetId, BlockNumber>> Pro
 mod tests {
 	use super::*;
 
+	struct MockRandomness;
+
+	impl Randomness<u32, u32> for MockRandomness {
+		fn random(subject: &[u8]) -> (u32, u32) {
+			(2 * subject[0] as u32, 0)
+		}
+	}
+
+	type MockRandomAssetIdProvider = RandomAssetIdProvider<u32, u32, MockRandomness>;
+
 	#[test]
 	fn incrementing_asset_id_provider_works() {
 		assert_eq!(IncrementingAssetIdProvider::next_asset_id(&0u32), Some(1));
 		assert_eq!(IncrementingAssetIdProvider::next_asset_id(&1u32), Some(2));
 
 		assert_eq!(IncrementingAssetIdProvider::next_asset_id(&u32::MAX), None);
+	}
+
+	#[test]
+	fn random_asset_id_provider_works() {
+		// This test shows that the input is indeed used as a subject for creating the output.
+		// But the safety and reasonable uniqueness guarantees depend on the actual
+		// randomness source used.
+		assert_eq!(MockRandomAssetIdProvider::next_asset_id(&10 ), Some(20));
 	}
 }
