6b7720965aab123b2318bf08141dca7ba7a1e676
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-03-07 16:27:51 +0100

    clippy

--- sage/example-transition/src/transition/mod.rs
+++ sage/example-transition/src/transition/mod.rs
@@ -123,7 +123,7 @@ where
 	}
 
 	fn generate_asset_id() -> Result<AssetId, TransitionError> {
-		Sage::create_next_asset_id().ok_or_else(|| TransitionError::CouldNotCreateAssetId)
+		Sage::create_next_asset_id().ok_or(TransitionError::CouldNotCreateAssetId)
 	}
 
 	fn verify_transition_rules(
