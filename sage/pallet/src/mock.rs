cce211f88b03ff86d456df74c2ab1d37af3a6ca8
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-04-22 12:08:26 +0200

    [pallet-sage] move `run_to_block` helper to mock.rs

--- sage/pallet/src/mock.rs
+++ sage/pallet/src/mock.rs
@@ -370,3 +370,17 @@ impl ExtBuilder {
 pub fn new_test_ext() -> sp_io::TestExternalities {
 	ExtBuilder::default().build()
 }
+
+pub fn run_to_block(n: u64) {
+	while System::block_number() < n {
+		if System::block_number() > 1 {
+			Randomness::on_finalize(System::block_number());
+			Sage::on_finalize(System::block_number());
+			System::on_finalize(System::block_number());
+		}
+		System::set_block_number(System::block_number() + 1);
+		System::on_initialize(System::block_number());
+		Sage::on_initialize(System::block_number());
+		Randomness::on_initialize(System::block_number());
+	}
+}
